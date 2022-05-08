d900972353fa2991c7d8604a6ee47ff07fd5403c
hackyeon <skek933@gmail.com>
2022-05-08 12:54:39 +0900

    compose_layout_first_commit
    compose_layout_first_commit

---
+++ app/src/main/java/com/hackyeon/compose_layout/ScaffoldActivity.kt
@@ -0,0 +1,64 @@
+package com.hackyeon.compose_layout
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hackyeon.compose_layout.ui.theme.Compose_layoutTheme
+
+class ScaffoldActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose_layoutTheme {
+                LayoutCodeLab()
+            }
+        }
+    }
+}
+
+@Composable
+fun LayoutCodeLab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutCodeLab")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier){
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LayoutCodeLabPreView() {
+    Compose_layoutTheme {
+        LayoutCodeLab()
+    }
+}
